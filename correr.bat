8b2f67688f508b735788bbbce177fb341a05b4f4
eschats <estebanchato@gmail.com>
2022-11-29 19:26:12 -0600

    cambio

--- correr.bat
+++ correr.bat
@@ -1,4 +1,4 @@
 call C:\Users\ESTEB\anaconda3\condabin\activate.bat tensorflow
-cd "C:\ProgramData\Jenkins\.jenkins\workspace\ProyectoQuinela"
+cd "C:\ProgramData\Jenkins\.jenkins\workspace\ProyectoQuinela\"
 python main.py
 pause
